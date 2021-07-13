4c3f8ac4f2684f03eeb2c569ca4a53bcf4f20577
mramdevputram <matang@topsinfosolutions.com>
2021-07-13 22:53:59 +0530

    issue fixes

--- server/config.js
+++ server/config.js
@@ -1,17 +1,11 @@
 const config = {
     dbName: 'SQLProperty',
-    dbUser: 'root',
+    dbUser: 'admin',
     dbPass: 'Mats@123',
     conn:{
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-    }
+        port: 3306
 }
 }
 module.exports = config
